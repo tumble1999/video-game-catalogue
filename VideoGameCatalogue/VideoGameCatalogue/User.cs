50170e17b9719a22b98144b6732c5a0b4d06fa8a
Cameron Trow <10trow.c@thestudio-liverpool.net>
2016-10-12 10:41:56 +0100

    User ID Get

--- VideoGameCatalogue/VideoGameCatalogue/User.cs
+++ VideoGameCatalogue/VideoGameCatalogue/User.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using VideoGameCatalogue.Properties;
 
 namespace VideoGameCatalogue
 {
@@ -16,7 +19,38 @@ namespace VideoGameCatalogue
         {
             this.username = username;
             this.password = password;
-            id = 
+            id = getID(username, password);
+
+            if (id == -1) {
+
+            }
+        }
+
+        private static int getID(string u, string p)
+        {
+            int output = -1;
+
+            OleDbConnection conn = new OleDbConnection(new Settings().VGCConnectionString);
+            string sql = "SELECT * FROM Users";
+            OleDbCommand cmd = new OleDbCommand(sql, conn);
+            conn.Open();
+
+            OleDbDataReader reader;
+            reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                if(reader.GetString(1) == u && reader.GetString(2) == p)
+                {
+                    output = reader.GetInt32(0);
+                    break;
+                }
+            }
+            
+            reader.Close();
+            conn.Close();
+                return output;
+            
         }
     }
 }
