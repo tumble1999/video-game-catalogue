29dd19151865bdb06a9f3261ec6bda3118c7445a
Cameron Trow <10trow.c@thestudio-liverpool.net>
2017-01-31 09:03:14 +0000

    added scroll

--- VideoGameCatalogue/VideoGameCatalogue/GameList.Designer.cs
+++ VideoGameCatalogue/VideoGameCatalogue/GameList.Designer.cs
@@ -366,7 +366,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoScroll = true;
+            this.AutoScrollMargin = new System.Drawing.Size(0, 24);
             this.ClientSize = new System.Drawing.Size(1096, 550);
             this.Controls.Add(this.gameItemPanel);
             this.Controls.Add(this.statusBar);
